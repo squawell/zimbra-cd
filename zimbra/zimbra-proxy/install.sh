b408db841b85bc4ffd68203b279e53c0de8ae4bc
Arvin <arvin_corpuz@dlsu.edu.ph>
2017-05-11 19:33:16 +0800

    admin on proxy

--- zimbra/zimbra-proxy/install.sh
+++ zimbra/zimbra-proxy/install.sh
@@ -100,7 +100,7 @@ echo "Restart CROND"
 service crond restart
 
 echo "opening PROXY Ports"
-sudo su -c "/opt/zimbra/libexec/zmproxyconfig -e -w -o -H ${HOSTNAME}.proxy-service.default.svc.cluster.local" -s /bin/sh zimbra
+sudo su -c "/opt/zimbra/libexec/zmproxyconfig -e -w -o -C -H ${HOSTNAME}.proxy-service.default.svc.cluster.local" -s /bin/sh zimbra
 
 echo "Server is ready..."
 if [[ $1 == "-d" ]]; then
